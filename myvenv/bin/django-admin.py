c3e805b41b113db4233ed961d5cbb78e253cac5e
JunLee <jhlee9870@gmail.com>
2016-07-13 23:37:34 +0900

    init

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/JunLee/Desktop/JunLee/Task/BeautyPlatform/server-project/goriserver/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
